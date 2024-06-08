fa51051a05e4636fba5fb4da6f52c7655d11382e
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2024-06-08 10:40:24 +0200

    Button

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import Windows1, wx
 
+class WorkWindows1(Windows1.MyWindow1):
+    def OnBtnSaveClicked( self, event ):
+        print("Button gedrückt")
+        event.Skip()
+
+
+
 class MainApp(wx.App):
     def OnInit(self):
-       self.fenster1 = Windows1.MyWindow1(None)
+       self.fenster1 = WorkWindows1(None)
        self.fenster1.Show(True) 
        return True
-    
+
 if __name__ == '__main__':
     app = MainApp()
     app.MainLoop()
